1830118ddbd9005890f33f4fe9c594616d63329b
Charlie12290 <68083273+Charlie12290@users.noreply.github.com>
2020-09-24 19:19:17 -0400

    Add project files.

---
+++ Assignment1_N01452091/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Assignment1_N01452091.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Assignment1_N01452091/Controllers/SubFiftyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1_N01452091.Controllers
+{
+    public class SubFiftyController : ApiController
+    {
+        //GET /api/SubFifty/40 -> -10
+        public int Get(int id) 
+        {
+
+            return (id - 50);
+        }
+    }
+}
